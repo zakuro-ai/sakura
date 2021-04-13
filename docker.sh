cca455f72403500e75ce98780524eee27ae6a80c
CADIC Jean-Maximilien <j.cadic@protonmail.ch>
2021-04-13 13:02:52 +0900

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 docker rmi -f zakuroai/sakura
 docker build . -t zakuroai/sakura
 docker run --rm \
-  #--gpus all \
-  #--shm-size=70g \
+  --gpus all \
+  --shm-size=70g \
   -v $(pwd)/data:/workspace/data -v $(pwd)/mnist_demo:/workspace/mnist_demo -it zakuroai/sakura python -m mnist_demo
